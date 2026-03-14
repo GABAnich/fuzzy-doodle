f7770dc2e47da1b096d0c9364a00332894e5a214
Ross <doofixxx@gmail.com>
2026-03-14 10:51:07 +0000

    RealRating -> TrueSkill

--- go/main.go
+++ go/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 type Player struct {
   Id string
-  RealRating int
+  TrueSkill int
   GlickoRating int
 }
 
@@ -13,29 +13,29 @@ type Team struct {
   Players []Player
 }
 
-func getTeamRealRating(t Team) int {
+func getTeamTrueSkill(t Team) int {
   total := 0
   for _, player := range t.Players {
-    total += player.RealRating
+    total += player.TrueSkill
   }
   return total
 }
 
 func game(t1, t2 Team) []Player {
   res := []Player{}
-  if (getTeamRealRating(t1) >= getTeamRealRating(t1)) {
+  if (getTeamTrueSkill(t1) >= getTeamTrueSkill(t1)) {
     for _, player := range t1.Players {
-      res = append(res, Player{Id: player.Id, RealRating: player.RealRating, GlickoRating: player.GlickoRating + 25})
+      res = append(res, Player{Id: player.Id, TrueSkill: player.TrueSkill, GlickoRating: player.GlickoRating + 25})
     }
     for _, player := range t2.Players {
-      res = append(res, Player{Id: player.Id, RealRating: player.RealRating, GlickoRating: player.GlickoRating - 25})
+      res = append(res, Player{Id: player.Id, TrueSkill: player.TrueSkill, GlickoRating: player.GlickoRating - 25})
     }
   } else {
     for _, player := range t1.Players {
-      res = append(res, Player{Id: player.Id, RealRating: player.RealRating, GlickoRating: player.GlickoRating - 25})
+      res = append(res, Player{Id: player.Id, TrueSkill: player.TrueSkill, GlickoRating: player.GlickoRating - 25})
     }
     for _, player := range t2.Players {
-      res = append(res, Player{Id: player.Id, RealRating: player.RealRating, GlickoRating: player.GlickoRating + 25})
+      res = append(res, Player{Id: player.Id, TrueSkill: player.TrueSkill, GlickoRating: player.GlickoRating + 25})
     }
   }
   return res
@@ -48,21 +48,21 @@ func main() {
   teamA := Team{
     Id: "Radiant",
     Players: []Player{
-      Player{Id: "a_pos1", RealRating: 4000, GlickoRating: 4200},
-      Player{Id: "a_pos2", RealRating: 4500, GlickoRating: 3800},
-      Player{Id: "a_pos3", RealRating: 2000, GlickoRating: 3500},
-      Player{Id: "a_pos4", RealRating: 3000, GlickoRating: 3500},
-      Player{Id: "a_pos5", RealRating: 3000, GlickoRating: 3500},
+      Player{Id: "a_pos1", TrueSkill: 4000, GlickoRating: 4200},
+      Player{Id: "a_pos2", TrueSkill: 4500, GlickoRating: 3800},
+      Player{Id: "a_pos3", TrueSkill: 2000, GlickoRating: 3500},
+      Player{Id: "a_pos4", TrueSkill: 3000, GlickoRating: 3500},
+      Player{Id: "a_pos5", TrueSkill: 3000, GlickoRating: 3500},
     },
   }
   teamB := Team{
     Id: "Dire",
     Players: []Player{
-      Player{Id: "b_pos1", RealRating: 5000, GlickoRating: 5200},
-      Player{Id: "b_pos2", RealRating: 5500, GlickoRating: 4800},
-      Player{Id: "b_pos3", RealRating: 3000, GlickoRating: 4500},
-      Player{Id: "b_pos4", RealRating: 4000, GlickoRating: 4500},
-      Player{Id: "b_pos5", RealRating: 4000, GlickoRating: 4500},
+      Player{Id: "b_pos1", TrueSkill: 5000, GlickoRating: 5200},
+      Player{Id: "b_pos2", TrueSkill: 5500, GlickoRating: 4800},
+      Player{Id: "b_pos3", TrueSkill: 3000, GlickoRating: 4500},
+      Player{Id: "b_pos4", TrueSkill: 4000, GlickoRating: 4500},
+      Player{Id: "b_pos5", TrueSkill: 4000, GlickoRating: 4500},
     },
   }
 
